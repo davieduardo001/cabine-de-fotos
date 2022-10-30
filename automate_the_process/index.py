3d733c9f4551339277851f2bee4ef8e308d0c9aa
davieduardo001 <ed.miranda@tutanota.com>
2022-10-30 19:00:48 -0300

    timer and location for the base

--- automate_the_process/index.py
+++ automate_the_process/index.py
@@ -15,24 +15,25 @@ import pyautogui as gui
 from time import sleep
 
 #go to DISPLAY button
-gui.moveTo(x=41, y=137, duration=0.1)
+sleep(3)
+gui.moveTo( x=30, y=152, duration=0.1)
 gui.click()
 
 #go to DOWNLOAD button
-gui.moveTo(x=125, y=137, duration=0.1)
+gui.moveTo(x=115, y=154, duration=0.1)
 gui.click()
 
 #go to transfere to CANVA
-sleep(1)
+sleep(1.5)
 gui.keyDown('ctrlleft')
 gui.press('tab')
 gui.keyUp('ctrlleft')
 #take the image from the "galery"
-gui.moveTo(x=224, y=264, duration=0.1)
+gui.moveTo(x=246, y=280, duration=0.1)
 gui.click()
 sleep(1)
 gui.press('enter')
-sleep(1)
+sleep(2)
 
 #return to the MAIN tab
 gui.keyDown('ctrlleft')
